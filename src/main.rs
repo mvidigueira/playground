006be671445457ca19f39a56801ef35f0b23bac1
Manuel <mvidigueira@gmail.com>
2021-12-09 18:58:47 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::time::Instant;
 
 use talk::crypto::KeyChain;
 use talk::net::SessionConnector;
-use talk::link::rendezvous::Connector as RendezvousConnector;
+use talk::link::rendezvous::Listener as RendezvousListener;
 use talk::link::rendezvous::Client;
+use talk::net::SessionListener;
 
     
 #[tokio::main]
@@ -13,21 +14,17 @@ async fn main() {
 
     let server = "3.15.200.1:9000";
 
-    let connector = RendezvousConnector::new(server, keychain, Default::default());
+    let listener = RendezvousListener::new(server, keychain, Default::default()).await;
 
     let client = Client::new(server, Default::default());
 
     client.publish_card(my_card.clone(), Some(0)).await.unwrap();
-    let shard = client.get_shard(0).await.unwrap();
 
-    let other = shard.into_iter().filter(|keycard| keycard.identity() != my_card.identity()).next().unwrap();
+    let mut listener = SessionListener::new(listener);
 
-    let connector = SessionConnector::new(connector);
-
-    let mut session = connector.connect(other.identity()).await.unwrap();
+    let (_, mut session) = listener.accept().await;
 
     let bytes = 1_000_000;
-    let message = (0..bytes/4).collect::<Vec<u32>>();
 
     let mut start = Instant::now();
     for i in 1.. {
@@ -36,8 +33,6 @@ async fn main() {
             println!("Throughput: {} BPS.", bytes as u128 / (end.duration_since(start).as_millis() / 1000));
             start = end;
         }
-        session.send(&message).await.unwrap();
+        let _message = session.receive::<Vec<u32>>().await.unwrap();
     }
-
-
 }
